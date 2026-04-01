4e320ec8fe21943ecbe14b5e03964d7a668f0b77
Ade Ariawan <=ariawan.ade@gmail.com>
2026-04-01 07:12:49 +0700

    fix: force 1cm top padding for print view in media print

--- resources/views/admin/exams/print-questions.blade.php
+++ resources/views/admin/exams/print-questions.blade.php
@@ -46,7 +46,10 @@
             }
             .page-wrapper {
                 max-width: 100%;
-                padding: 0 !important;
+                padding-top: 1cm !important;
+                padding-left: 0 !important;
+                padding-right: 0 !important;
+                padding-bottom: 0 !important;
                 box-shadow: none !important;
             }
         }
